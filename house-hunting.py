8154ab30cb40153bb6de6eea9b244197ad119850
bennykoval <bethanykoval@gmail.com>
2019-06-03 13:28:58 -0400

    Well that didn't work. Delete hard code of savings 0

--- house-hunting.py
+++ house-hunting.py
@@ -1,5 +1,4 @@
 portion_down_payment = 0.25
-current_savings = 0
 r = 0.04
 
 annual_salary_as_str = input("Enter your annual salary: ")
@@ -11,8 +10,9 @@ cost_of_home = int(cost_of_home_as_str)
 
 if annual_salary > 0:
     (annual_salary * portion_down_payment) == current_savings
-    (current_savings + (current_savings * r/12)) == num_months
 else: 
-    print("You have no money to save! Sorry.")
+    print("You have no income to save! Sorry.")
+
+
 
 print("Number of months: ", num_months)
