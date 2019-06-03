3273dd7b639b0068127fe09a60561b72e4f09299
bennykoval <bethanykoval@gmail.com>
2019-06-03 16:02:24 -0400

    Spacing

--- house-hunting.py
+++ house-hunting.py
@@ -5,8 +5,10 @@ months = 0
 
 annual_salary_as_str = input("Enter your annual salary: ")
 annual_salary = int(annual_salary_as_str)
+
 portion_saved_as_str = input("Enter the percent of your salary to save, as a decimal: ")
 portion_saved = float(portion_saved_as_str)
+
 cost_of_home_as_str = input("Enter the cost of your dream home: ")
 cost_of_home = int(cost_of_home_as_str)
 
@@ -14,6 +16,7 @@ down_cost = cost_of_home * portion_down_payment
 
 if annual_salary > 0:
     monthly_savings = (annual_salary / 12) * portion_saved
+
 else: 
     print("Sorry - you have no income to save!")
 
