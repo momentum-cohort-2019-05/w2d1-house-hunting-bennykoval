d5d820e5e0022e19b392496206909b3ce5632b4c
bennykoval <bethanykoval@gmail.com>
2019-06-03 12:59:46 -0400

    Fix typo

--- house-hunting.py
+++ house-hunting.py
@@ -10,4 +10,4 @@ cost_of_home_as_str = input("Enter the cost of your dream home: ")
 cost_of_home = int(cost_of_home_as_str)
 num_months = (annual_salary * portion_down_payment) + (current_savings * r / 12)
 
-print("Your total cost is", num_months)
+print("Number of months: ", num_months)
